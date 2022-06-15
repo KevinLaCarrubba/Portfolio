ce3e6ca6db4781b4ae0d0e0eb7db8cf7b47c4ef5
Kevin LaCarrubba <kevinlacarrubba@KevinsMacStudio.fios-router.home>
2022-06-14 22:24:05 -0400

    flipper flipping flips

--- src/Pages/AboutPage/index.js
+++ src/Pages/AboutPage/index.js
@@ -20,9 +20,64 @@ const AboutPage = () => {
                     <h1 className={styles.hello}>
                         Hey, I'm <span className={styles.myName}>Kevin</span>.
                     </h1>
+                    <div className={styles.myInfo}>
+                        <p>
+                            Junior Front-End Developer<span>.</span>{' '}
+                        </p>
+                        <p>
+                            Word<span>.</span>{' '}
+                        </p>
+                        <p>
+                            Word<span>.</span>{' '}
+                        </p>
+                        <p>
+                            Word<span>.</span>{' '}
+                        </p>
+                    </div>
                 </div>
             </section>
-            <section className={styles.cardBox}>Do it up big here</section>
+            <section className={styles.cardBox}>
+                <div className={styles.flipBox}>
+                    <div className={styles.flipBoxInner}>
+                        <div className={styles.frontside}>
+                            <h2>Hobbies</h2>
+                        </div>
+                        <div className={styles.backside}>
+                            <h1>SHOTS !</h1>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.flipBox}>
+                    <div className={styles.flipBoxInner}>
+                        <div className={styles.frontside}>
+                            <h2>Education</h2>
+                        </div>
+                        <div className={styles.backside}>
+                            <h1>I KNOW SOME SHIT!</h1>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.flipBox}>
+                    <div className={styles.flipBoxInner}>
+                        <div className={styles.frontside}>
+                            <h2>Preferences</h2>
+                        </div>
+                        <div className={styles.backside}>
+                            <h1>WORK FROM HOME!</h1>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.flipBox}>
+                    <div className={styles.flipBoxInner}>
+                        <div className={styles.frontside}>
+                            <h2>Skills</h2>
+                        </div>
+                        <div className={styles.backside}>
+                            <h1>FIGURE IT OUT!</h1>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
