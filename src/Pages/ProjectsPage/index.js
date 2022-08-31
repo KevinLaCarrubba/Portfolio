c90a362d1932d133b9781793c4fcfdbc2c9e1a72
Kevin LaCarrubba <kevinlacarrubba@KevinsMacStudio.fios-router.home>
2022-08-31 15:21:55 -0400

    Added projects idea

--- src/Pages/ProjectsPage/index.js
+++ src/Pages/ProjectsPage/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from './ProjectsPage.module.scss'
+import WeatherApp from '../../images/WeatherApp.png'
 
 const ProjectsPage = () => {
     return (
@@ -9,7 +10,34 @@ const ProjectsPage = () => {
                 <p>A couple of my </p>
                 <p>personal projects</p>
             </div>
-            <div className={styles.rightSide}>Projects go here</div>
+            <div className={styles.rightSide}>
+                <div className={styles.appView}>
+                    <h4 className={styles.appName}>Weather App</h4>
+                    <div className={styles.infoContainer}>
+                        <img
+                            className={styles.appImage}
+                            src={WeatherApp}
+                            alt="Weather App Display"
+                        />
+                        <div className={styles.tagBox}>
+                            <div className={styles.tag}>React</div>
+                            <div className={styles.tag}>Sass</div>
+                        </div>
+                        <div className={styles.siteLinks}>
+                            <div className={styles.link}>
+                                <a href="https://www.google.com">
+                                    View Github Repo
+                                </a>
+                            </div>
+                            <div className={styles.link}>
+                                <a href="https://www.google.com">
+                                    Launch Live Demo
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
