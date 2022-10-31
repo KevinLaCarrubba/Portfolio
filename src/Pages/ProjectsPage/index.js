fd3a9e9193652a8e008412b7d6fe8b693025eff5
Kevin <kevin.lacarrubba@yahoo.com>
2022-10-31 17:31:53 -0400

    added projects to page

--- src/Pages/ProjectsPage/index.js
+++ src/Pages/ProjectsPage/index.js
@@ -1,8 +1,32 @@
 import React from 'react'
 import styles from './ProjectsPage.module.scss'
-import WeatherApp from '../../images/WeatherApp.png'
+import { photo, weather, calculator } from '../../images/photo'
 
 const ProjectsPage = () => {
+    const Projects = [
+        {
+            name: 'Weather App',
+            url: 'https://luxury-yeot-4a744e.netlify.app/',
+            image: weather,
+            github: 'https://github.com/KevinLaCarrubba/weather-app',
+            tags: ['React', 'Sass']
+        },
+        {
+            name: 'Photography Site',
+            url: 'https://steady-macaron-986588.netlify.app/',
+            image: photo,
+            github: 'https://github.com/KevinLaCarrubba/weather-app',
+            tags: ['NextJs', 'TypeScript', 'Sass']
+        },
+        {
+            name: 'Calculator',
+            url: 'https://steady-macaron-986588.netlify.app/',
+            image: calculator,
+            github: 'https://github.com/KevinLaCarrubba/weather-app',
+            tags: ['NextJs', 'TypeScript', 'Sass']
+        }
+    ]
+
     return (
         <div className={styles.container}>
             <div className={styles.leftSide}>
@@ -11,34 +35,45 @@ const ProjectsPage = () => {
                 <p>personal projects</p>
             </div>
             <div className={styles.rightSide}>
-                <div className={styles.appView}>
-                    <h4 className={styles.appName}>Weather App</h4>
-                    <div className={styles.infoContainer}>
-                        <a href="https://luxury-yeot-4a744e.netlify.app/">
-                            <img
-                                className={styles.appImage}
-                                src={WeatherApp}
-                                alt="Weather App Display"
-                            />
-                        </a>
-                        <div className={styles.tagBox}>
-                            <div className={styles.tag}>React</div>
-                            <div className={styles.tag}>Sass</div>
-                        </div>
-                        <div className={styles.siteLinks}>
-                            <div className={styles.link}>
-                                <a href="https://github.com/KevinLaCarrubba/weather-app">
-                                    View Github Repo
+                {Projects.map((data, index) => {
+                    return (
+                        <div className={styles.appView}>
+                            <h4 className={styles.appName}>{data.name}</h4>
+                            <div className={styles.infoContainer}>
+                                <a href={data.url}>
+                                    <img
+                                        className={styles.appImage}
+                                        src={data.image}
+                                        alt="Weather App Display"
+                                    />
                                 </a>
-                            </div>
-                            <div className={styles.link}>
-                                <a href="https://luxury-yeot-4a744e.netlify.app/">
-                                    Launch Live Demo
-                                </a>
+                                <div className={styles.tagBox}>
+                                    {data.tags.map((data, index) => {
+                                        return (
+                                            <div
+                                                key={index}
+                                                className={styles.tag}>
+                                                {data}
+                                            </div>
+                                        )
+                                    })}
+                                </div>
+                                <div className={styles.siteLinks}>
+                                    <a href={data.github}>
+                                        <div className={styles.link}>
+                                            View Github Repo
+                                        </div>
+                                    </a>
+                                    <a href={data.url}>
+                                        <div className={styles.link}>
+                                            Launch Live Demo
+                                        </div>
+                                    </a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
+                    )
+                })}
             </div>
         </div>
     )
---
+++ src/images/photo.js
@@ -0,0 +1,7 @@
+import WeatherApp from './WeatherApp.png'
+import Photosite from './Photosite.png'
+import Calculator from './Calculator.png'
+
+export const photo = Photosite
+export const weather = WeatherApp
+export const calculator = Calculator
